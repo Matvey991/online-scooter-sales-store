c957518725bf28827f7b46b957f120a4a059de64
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-07-31 21:45:48 +0400

    project

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -2,10 +2,12 @@ import styles from './style.module.css'
 
 export const HeaderMenu = () => {
     return (
+        <div className={styles.headerMenuH}>
     <div className={styles.headerMenu}>
 <div className={styles.headerMenuHeading}>
 <h1 className={styles.headerMenuHeading}>KUGGO</h1>
 </div>
+    </div>
     </div>
     )
 }
