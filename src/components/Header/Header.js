e0acbfbf389236cbf23f933f9e423424cdd1ab09
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-08-05 23:49:57 +0400

    project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,44 @@
-import { HeaderTop } from "./HeaderTopBlock"
 import styles from './Header.module.css'
-import { HeaderMenu } from "../HeaderMenu/HeaderMenu"
+import {ReactComponent as Viber} from '../../helpers/viber.svg'
+import {ReactComponent as Tg} from '../../helpers/tg.svg'
+import {ReactComponent as What} from '../../helpers/what.svg'
+import {ReactComponent as Number} from '../../helpers/numbers.svg'
+
 
 export const Header = () => {
     return (
+        <>
+<header>
 <div className={styles.header}>
-    <HeaderTop/>
-    <HeaderMenu/>
+<div className={styles.container}>
+<button>Сервис</button>
+<button>Сотрудничество</button>
+<button>Заказать звонок</button>
+<ul>
+<li><Viber/></li>
+<li><Tg/></li>
+<li><What/></li>
+</ul>
+<a className={styles.headerNumber} href='#!'>+7 (800) 505-54-61</a>
+<ul>
+<li><Number/></li>
+</ul>
 </div>
+</div>
+</header>
+<section>
+    <div className={styles.section}>
+        <div className={styles.sectionMenu}>
+            <div className={styles.sectionH2}>
+        <h2>
+        Kugoo
+        </h2>
+    </div>
+    </div>
+        <button className={styles.sectionButton}>Каталог</button>
+    </div>
+</section>
+</>
+
     )
 }
