d2bb9b445643c629bbbb3d69cc4df1ae74b70c64
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-08-09 00:56:43 +0400

    project

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,14 +1,24 @@
 import styles from './main.module.css'
 import {ReactComponent as Rectangle} from '../../helpers/Rectangle.svg'
+import {ReactComponent as Samokat} from '../../helpers/samokat.svg'
 const Main = () => {
     return (
         <>
          <main>
             <div className={styles.mainBackround}>
-            <Rectangle/>
+            <Rectangle/> 
+            <Samokat className={styles.mainSamokat}/>
             </div>
             <div className={styles.mainButtonAbove}>
             <button className={styles.mainButton}>Новинка</button>
+            </div>
+            <div className={styles.mainDownHeading}>
+            <h1 className={styles.mainHeading}>
+            Электросамокаты Kugoo Kirin<br/> от официального дилера
+            </h1>
+            <div className={styles.mainParagraph}>
+            <p>с бесплатной доставкой по РФ от 1 дня</p>
+            </div>
             </div>
         </main>   
         </>
