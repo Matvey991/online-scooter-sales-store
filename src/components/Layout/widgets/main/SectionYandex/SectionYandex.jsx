155f2c02cea27bef1ff944d6943b67762a5dbe2a
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-10-10 16:24:41 +0400

    main

--- src/components/Layout/widgets/main/SectionYandex/SectionYandex.jsx
+++ src/components/Layout/widgets/main/SectionYandex/SectionYandex.jsx
@@ -3,33 +3,29 @@ import styles from "./SectionYandex.module.css";
 
 export const SectionYandex = () => {
   return (
-    <section>
+    <section className={styles.section}>
       <div className={styles.AdditionalInformationParagraphs}>
         <div className={styles.AdditionalInformationNumber1}>
-          <h2 className={styles.footerHeadingtextNumber1}>ГАРАНТИЯ 1 ГОД</h2>
-          <p className={styles.footerParagraphNumber1}>на весь ассортимент</p>
+          <h2 className={styles.HeadingtextNumber1}>ГАРАНТИЯ 1 ГОД</h2>
+          <p className={styles.ParagraphNumber1}>на весь ассортимент</p>
         </div>
-        <div className={styles.footerAdditionalInformationNumber2}>
-          <h1 className={styles.footerHeadingtextNumber2}>РАССРОЧКА</h1>
-          <p className={styles.footerParagraphNumber2}>от 6 месяцев</p>
+        <div className={styles.AdditionalInformationNumber2}>
+          <h1 className={styles.HeadingtextNumber2}>РАССРОЧКА</h1>
+          <p className={styles.ParagraphNumber2}>от 6 месяцев</p>
         </div>
-        <div className={styles.footerAdditionalInformationNumber3}>
-          <h1 className={styles.footerHeadingtextNumber3}>ПОДАРКИ</h1>
-          <p className={styles.footerParagraphNumber3}>и бонусам к покупкам </p>
+        <div className={styles.AdditionalInformationNumber3}>
+          <h1 className={styles.HeadingtextNumber3}>ПОДАРКИ</h1>
+          <p className={styles.ParagraphNumber3}>и бонусам к покупкам </p>
         </div>
-        <div className={styles.footerAdditionalInformationNumber4}>
-          <div className={styles.footerYandexElements}>
-            <div className={styles.footerBackroundYandex}>
-              <img
-                src="/Glyph.svg"
-                alt="."
-                className={styles.footerYandexLogo}
-              />
+        <div className={styles.AdditionalInformationNumber4}>
+          <div className={styles.YandexElements}>
+            <div className={styles.BackroundYandex}>
+              <img src="/Glyph.svg" alt="." className={styles.YandexLogo} />
             </div>
-            <div className={styles.footerYandexRatingAndParagraph}>
-              <p className={styles.footerYandexParagraph}>Яндекс отзывы</p>
-              <div className={styles.footerYandexStarAndHeading}>
-                <img src="/Star.svg" alt="." className={styles.footerStar} />
+            <div className={styles.YandexRatingAndParagraph}>
+              <p className={styles.YandexParagraph}>Яндекс отзывы</p>
+              <div className={styles.YandexStarAndHeading}>
+                <img src="/Star.svg" alt="." className={styles.Star} />
                 <h1>4,9</h1>
               </div>
             </div>
