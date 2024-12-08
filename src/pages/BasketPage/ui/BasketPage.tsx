fa8cecc769f1e7ec882040eddf3607ec7f7c0e50
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-12-08 23:15:55 +0400

    fix basket

--- src/pages/BasketPage/ui/BasketPage.tsx
+++ src/pages/BasketPage/ui/BasketPage.tsx
@@ -3,6 +3,6 @@ import cls from './BasketPage.module.css'
 
 export const BasketPage = () => {
   return <div className={cls.BasketPage}>
-
+    <h1 className={cls.basketHeading}>МОЯ КОРЗИНА</h1>
     </div>;
 };
